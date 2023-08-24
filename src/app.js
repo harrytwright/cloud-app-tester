42467c1843e7b3d912a68c6cb96a428124e22019
harrytwright <haroldtomwright@gmail.com>
2023-08-24 17:28:54 +0100

    src: Connect persistent redis datastore
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const { createClient } = require('redis')
 const bodyParser = require('body-parser')
 const timeout = require('connect-timeout')
+const log = require('@harrytwright/logger')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const client = createClient();
+
+client.on('error', err =>
+  log.error('redis', err, 'Redis Client Error')
+);
 
 const traverse = (buffer) => {
   let data = ''
@@ -25,14 +33,14 @@ const traverse = (buffer) => {
 
 const fixBodyParser = (error, req, res, next) => {
   if (error instanceof SyntaxError) {
-    console.log(error.body)
-
-    // try {
-    //   req.body = JSON.parse(traverse(error.body))
-    //   return next()
-    // } catch (_) {
-    //   return next(error)
-    // }
+    log.warn('body-parser', error, 'Failed to parse body')
+    try {
+      req.body = JSON.parse(traverse(error.body))
+      return next()
+    } catch (err) {
+      log.warn('body-parser:fix', err, 'Failed to parse traversed body')
+      return next(error)
+    }
   }
 
   return next(error)
@@ -41,7 +49,7 @@ const fixBodyParser = (error, req, res, next) => {
 app.set('views', process.cwd() + '/public/views')
 app.set('view engine', 'ejs')
 
-app.use(require('morgan')('dev'))
+app.use(require('./morgan').morgan(log))
 app.use(bodyParser.json())
 app.use(fixBodyParser)
 
@@ -51,53 +59,70 @@ const homepage = (req, res, next) =>
 app.get('/', homepage)
 app.get('/homepage', homepage)
 
-const database = new Map([])
-
-app.patch('/config/:centre', timeout('5s'), (req, res) => {
-  if (database.has(req.params.centre)) {
-    database.get(req.params.centre).config = req.body
+app.patch('/config/:centre', timeout('5s'), async (req, res) => {
+  const { centre } = req.params
+  if (!await client.exists(centre)) {
+    res.status(201)
   } else {
-    database.set(req.params.centre, { config: req.body, items: [] })
+    res.status(200)
   }
 
-  return res.status(200).send('accepted')
+  await client.set(centre, JSON.stringify(req.body))
+
+  return res.send('accepted')
 })
 
-const missingCentre = (req, res, next) => {
+const missingCentre = async (req, res, next) => {
   const { centre } = req.params
-  if (!database.has(centre))
+  if (!await client.exists(centre))
     return res.status(404).json({ error: `Unable to find ${centre}` })
   return next()
 }
 
-app.get('/items/:centre', missingCentre, (req, res) => {
+app.get('/items/:centre', missingCentre, async (req, res) => {
   const { centre } = req.params
-  return res.status(200).json(database.get(centre).items)
+  const items = await client.hVals(`${centre}:items`)
+  return res.status(200).json(items.map((el) => JSON.parse(el)))
 })
 
-app.post('/items/:centre', missingCentre, (req, res) => {
+app.post('/items/:centre', missingCentre, async (req, res) => {
   const { centre } = req.params
   const { item_id } = req.body
 
-  database.get(centre).items.push(req.body)
+  await client.hSet(`${centre}:items`, item_id, JSON.stringify(req.body))
+
   return res.status(201).json({ id: item_id, item_id })
 })
 
-app.put('/items/:centre/:item', fixBodyParser, missingCentre, (req, res) => {
+app.put('/items/:centre/:item', missingCentre, async (req, res) => {
   const { centre, item } = req.params
+  const { item_id } = req.body
 
-  const idx = database.get(centre).items.findIndex((el) => el.item_id === item)
-  if (idx === -1)
+  if (item_id != item)
+    return res.status(412).json({ error: `The item id in the URI does not match the item_id in the body` })
+
+  if (!await client.hExists(`${centre}:items`, item))
     return res.status(404).json({ error: `Unable to find ${item}` })
 
-  database.get(centre).items[idx] = req.body
+  await client.hSet(`${centre}:items`, item, JSON.stringify(req.body))
 
   return res.status(200).json({ id: item, item_id: item })
 })
 
-app.get('/:centre', missingCentre, (req, res) => {
+app.delete('/items/:centre/:item', missingCentre, async (req, res) => {
+  const { centre, item } = req.params
+
+  if (!await client.hExists(`${centre}:items`, item))
+    return res.status(400).json({ error: `Unable to find ${item}` })
+
+  await client.hDel(`${centre}:items`, item)
+
+  return res.status(200).send('deleted')
+})
+
+app.get('/:centre', missingCentre, async (req, res) => {
   const { centre } = req.params
-  return res.status(200).json(database.get(centre).config)
+  return res.status(200).json(JSON.parse(await client.get(centre)))
 })
 
 app.get('/:centre/status', missingCentre, (req, res) => {
@@ -113,6 +138,11 @@ app.get('/:centre/status', missingCentre, (req, res) => {
   return res.status(200).json(body)
 })
 
-app.listen(port, () => {
-  console.log(`${process.title} listening on port ${port}`)
+client.connect().then(() => {
+  log.info('init', 'Connected to redis')
+
+  log.info('init', `Starting ${process.title}`)
+  app.listen(port, () => {
+    log.info('init', `${process.title} listening on port ${port}`)
+  })
 })
