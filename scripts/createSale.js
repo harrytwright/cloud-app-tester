ed926f72b08a369d1e861a0c52bf1e5c1f517051
harrytwright <haroldtomwright@gmail.com>
2023-09-01 14:48:50 +0100

    script: Update createSale.js
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

--- scripts/createSale.js
+++ scripts/createSale.js
@@ -1,18 +1,19 @@
 const { faker } = require('@faker-js/faker');
 
-let id = 0
+let id = 3
 
 const createSale = (id) => () => ({
   ID: id++,
   createdAt: (new Date()).toISOString(),
   area: 'Lanes',
   lane: faker.number.int({ min: 1, max: 12 }),
+  table: 0,
   name: faker.person.fullName(),
-  phone: faker.phone.number('07### ######'),
+  phone: faker.phone.number('+44 7### ######'),
   payments: [
-    { paid: 800, timestamp: (new Date()).toISOString(), payment_ref: faker.string.uuid() }
+    { paid: 800, timestamp: (new Date()).toISOString(), payment_ref: faker.string.numeric(16) }
   ],
-  items: [{ ID: 102 }, { ID: 104, modifier: 'true' }],
+  items: [{ ID: 102, quantity: 1, price: 800 }, { ID: 104, quantity: 1, price: 0, modifier: 'true' }],
   notes: 'Could I get some chili flakes if you have please'
 })
 
